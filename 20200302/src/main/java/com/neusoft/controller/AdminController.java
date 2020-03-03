4c956c3f4a03d9267098d5a046d7cd1128bcade6
neuedu-yhl <44873018+neuedu-yhl@users.noreply.github.com>
2020-03-03 09:58:48 +0800

    参数绑定

--- 20200302/src/main/java/com/neusoft/controller/AdminController.java
+++ 20200302/src/main/java/com/neusoft/controller/AdminController.java
@@ -1,7 +1,15 @@
 package com.neusoft.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AdminController {
@@ -9,11 +17,35 @@ public class AdminController {
 	// 一个方法接收一个请求
 	// 视图解析器
 	@RequestMapping("/admin.do")
-	public String admin() {
-		
-		return "WEB-INF/hello.html";
-		
+	@ResponseBody
+	public String admin(HttpServletRequest request,HttpServletResponse resp) {
+		String attribute = (String)request.getSession().getAttribute("username");
+		return attribute;	
 	}
+	
+	
+	@RequestMapping("/setSession.do")
+	public String setSession(HttpServletRequest request,HttpServletResponse resp) {
+		request.getSession().setAttribute("username", "zhangsan");
+		return "hello";	
+	}
+	
+	
+	// 参数绑定
+	@RequestMapping("/param.do")
+	@ResponseBody
+	public String paramBind(String username,String psw) {
+		return username+psw;
+	}
+	
+	
+	
+	// 访问html
+	@RequestMapping("/hello.do")
+	public String toHelloPage() {
+		return "hello";
+	}
+	
 	
 	
 	
